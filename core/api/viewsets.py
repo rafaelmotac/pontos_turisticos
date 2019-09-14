502e8150778e6a913e95b037d36ec7f06137d3dc
Rafael Mota Correia <rafael.correia@iviatecnologia.com.br>
2019-09-14 11:30:00 -0300

    Adding new custom actions to core api

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core.api.serializers import PontoTuristicoSerializer
@@ -8,8 +10,15 @@ class PontoTuristicoViewSet(ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    # queryset = PontoTuristico.objects.all()
+    queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
 
-    def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+    # def get_queryset(self):
+    #     return PontoTuristico.objects.filter(aprovado=True)
+
+    @action(methods=['get'], detail=False)
+    def aprovados(self, request):
+        pontos_aprovados = PontoTuristico.objects.filter(aprovado=True)
+        serializer = self.get_serializer(pontos_aprovados, many=True)
+        return Response(serializer.data)
+
